6ec5c15b3c8b919e3fc0aef1df33c5921cdea59f
Armatron44 <sandyjoarmstrong@hotmail.com>
2025-03-24 20:36:55 +0000

    fix: ParameterLike now constructed from refnxParameter or refl1dParameter conditionally.

--- src/vfp/typing.py
+++ src/vfp/typing.py
@@ -1,17 +1,23 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+# not clean but the following appears to work with refl1d + refnx present.
 type ParameterLike = float
 
 if TYPE_CHECKING:
-    try:
+    try: # if we have bumps, see if we can load in refnx too.
         from bumps.parameter import Parameter as bumpsParameter
-        type ParameterLike = ParameterLike | bumpsParameter
-    except ImportError as ie:
+        type ParameterLike = float | bumpsParameter
+        try:
+            from refnx.analysis import Parameter as refnxParameter
+            type ParameterLike = float | bumpsParameter | refnxParameter
+        except ImportError as ie:
+            print(f"{ie} compatible refnx package not installed.")
+    except ImportError as ie: # if we don't have bumps, try refnx.
         print(f"{ie} compatible refl1d & bumps packages not installed.")
-    
-    try:
-        from refnx.analysis import Parameter as refnxParameter
-        type ParameterLike = ParameterLike | refnxParameter
-    except ImportError as ie:
-        print(f"{ie} compatible refnx package not installed.")
+        try:
+            from refnx.analysis import Parameter as refnxParameter
+            type ParameterLike = float | refnxParameter
+        except ImportError as ie:
+            print(f"{ie} compatible refnx package not installed.")
+        
