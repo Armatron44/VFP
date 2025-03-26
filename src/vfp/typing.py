1f6c5a675764e4c1aae63ccc41bae2a6595162cf
Armatron44 <sandyjoarmstrong@hotmail.com>
2025-03-26 21:38:11 +0000

    fix: remove type_checking check in typing.py.

--- src/vfp/typing.py
+++ src/vfp/typing.py
@@ -1,23 +1,24 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
+"""
+Setup to define a type alias `ParameterLike` which is conditional
+on the available dependencies to `vfp`.
+"""
 
 # not clean but the following appears to work with refl1d + refnx present.
-type ParameterLike = float
+type ParameterLike = int | float
 
-if TYPE_CHECKING:
-    try: # if we have bumps, see if we can load in refnx too.
-        from bumps.parameter import Parameter as bumpsParameter
-        type ParameterLike = float | bumpsParameter
-        try:
-            from refnx.analysis import Parameter as refnxParameter
-            type ParameterLike = float | bumpsParameter | refnxParameter
-        except ImportError as ie:
-            print(f"{ie} compatible refnx package not installed.")
-    except ImportError as ie: # if we don't have bumps, try refnx.
-        print(f"{ie} compatible refl1d & bumps packages not installed.")
-        try:
-            from refnx.analysis import Parameter as refnxParameter
-            type ParameterLike = float | refnxParameter
-        except ImportError as ie:
-            print(f"{ie} compatible refnx package not installed.")
+try: # if we have bumps, see if we can load in refnx too.
+    from bumps.parameter import Parameter as bumpsParameter
+    type ParameterLike = int | float | bumpsParameter
+    try:
+        from refnx.analysis import Parameter as refnxParameter
+        type ParameterLike = int | float | bumpsParameter | refnxParameter
+    except ImportError as ie:
+        print(f"{ie} compatible refnx package not installed.")
+except ImportError as ie: # if we don't have bumps, try refnx.
+    print(f"{ie} compatible refl1d & bumps packages not installed.")
+    try:
+        from refnx.analysis import Parameter as refnxParameter
+        type ParameterLike = int | float | refnxParameter
+    except ImportError as ie:
+        print(f"{ie} compatible refnx package not installed.")
         
