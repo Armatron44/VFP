57f9a0fa9f0a0990c93b2e45185afb9b465f36ac
Armatron44 <sandyjoarmstrong@hotmail.com>
2025-03-28 22:25:36 +0000

    fix: add _BinaryOp and Expression to ParameterLike

--- src/vfp/typing.py
+++ src/vfp/typing.py
@@ -1,24 +1,27 @@
 """
 Setup to define a type alias `ParameterLike` which is conditional
-on the available dependencies to `vfp`.
+on the available dependencies to `vfp`. `ParameterLike` is the union
+of float, int, refnxParameter, _BinaryOp, bumpsParameter & Expression 
 """
 
 # not clean but the following appears to work with refl1d + refnx present.
 type ParameterLike = int | float
 
 try: # if we have bumps, see if we can load in refnx too.
-    from bumps.parameter import Parameter as bumpsParameter
-    type ParameterLike = int | float | bumpsParameter
+    from bumps.parameter import Parameter as bumpsParameter, Expression
+    type ParameterLike = int | float | bumpsParameter | Expression
     try:
         from refnx.analysis import Parameter as refnxParameter
-        type ParameterLike = int | float | bumpsParameter | refnxParameter
+        from refnx.analysis.parameter import _BinaryOp
+        type ParameterLike = int | float | bumpsParameter | Expression | refnxParameter | _BinaryOp
     except ImportError as ie:
         print(f"{ie} compatible refnx package not installed.")
 except ImportError as ie: # if we don't have bumps, try refnx.
     print(f"{ie} compatible refl1d & bumps packages not installed.")
     try:
         from refnx.analysis import Parameter as refnxParameter
-        type ParameterLike = int | float | refnxParameter
+        from refnx.analysis.parameter import _BinaryOp
+        type ParameterLike = int | float | refnxParameter | _BinaryOp
     except ImportError as ie:
         print(f"{ie} compatible refnx package not installed.")
         
