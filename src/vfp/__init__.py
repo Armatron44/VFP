d3d44f9bf218fe0a7376f5df2c9e6e49141fd8db
Armatron44 <sandyjoarmstrong@hotmail.com>
2025-04-27 20:26:04 +0100

    style: ruff, black, isort __init__.py

--- src/vfp/__init__.py
+++ src/vfp/__init__.py
@@ -1,20 +1,21 @@
-# read version from installed package
 from importlib.metadata import version
 
-__version__ = version("vfp")
+__version__ = version("vfp")  # read version from installed package
 
 from vfp.vfp import VFP
-__all__ = ["VFP"]
+
+__all__ = [VFP.__name__]
 
 try:
     from vfp.vfp import refnxVFP
 
-    __all__ = __all__ + ["refnxVFP"]
+    __all__.append(refnxVFP.__name__)
 except ImportError as ie:
     print(f"{ie} compatible refnx package not installed.")
 
 try:
     from vfp.vfp import refl1dVFP
-    __all__ = __all__ + ["refl1dVFP"]
+
+    __all__.append(refl1dVFP.__name__)
 except ImportError as ie:
     print(f"{ie} compatible refl1d package not installed.")
