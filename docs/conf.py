e485cc997c497a91d9704cef96bcffcf91897149
Armatron44 <sandyjoarmstrong@hotmail.com>
2025-07-06 10:14:39 +0100

    docs: simplify conf.py to use ext.mathjax

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,12 @@ author = "Alexander Armstrong"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
     "myst_nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    'sphinx_math_dollar', 
     'sphinx.ext.mathjax'
 ]
 autoapi_dirs = ["../src"]
@@ -34,19 +34,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
 
-mathjax_config = {
-    'tex2jax': {
-        'inlineMath': [ ["\\(","\\)"] ],
-        'displayMath': [["\\[","\\]"] ],
-    },
-}
+nb_execution_mode = "off"
 
-mathjax3_config = {
-  "tex": {
-    "inlineMath": [['\\(', '\\)']],
-    "displayMath": [["\\[", "\\]"]],
-  }
-}
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+]
