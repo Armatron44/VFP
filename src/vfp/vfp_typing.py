58444398691a696c6dd4f70d2959d192f9f1ca91
Armatron44 <sandyjoarmstrong@hotmail.com>
2025-07-25 23:12:14 +0100

    feat: add SLDConstraintType protocol + rename to avoid shadow typing module

--- src/vfp/vfp_typing.py
+++ src/vfp/vfp_typing.py
@@ -1,3 +1,5 @@
+from typing import Protocol
+
 """
 Define a type alias `ParameterLike` which is conditional on the available
 dependencies to `vfp`. `ParameterLike` is the union of `float`, `int`,
@@ -5,24 +7,42 @@ dependencies to `vfp`. `ParameterLike` is the union of `float`, `int`,
 `bumps.parameter.Parameter` & `bumps.parameter.Expression`
 """
 
-# not clean but the following appears to work with refl1d + refnx present.
 type ParameterLike = int | float
 
-try: # if we have bumps, see if we can load in refnx too.
-    from bumps.parameter import Parameter as bumpsParameter, Expression
+try:  # if we have bumps, see if we can load in refnx too.
+    from bumps.parameter import Expression
+    from bumps.parameter import Parameter as bumpsParameter
+
     type ParameterLike = int | float | bumpsParameter | Expression
     try:
         from refnx.analysis import Parameter as refnxParameter
         from refnx.analysis.parameter import _BinaryOp
-        type ParameterLike = int | float | bumpsParameter | Expression | refnxParameter | _BinaryOp
+
+        type ParameterLike = (
+            int
+            | float
+            | bumpsParameter
+            | Expression
+            | refnxParameter
+            | _BinaryOp
+        )
     except ImportError as ie:
         print(f"{ie} compatible refnx package not installed.")
-except ImportError as ie: # if we don't have bumps, try refnx.
+except ImportError as ie:  # if we don't have bumps, try refnx.
     print(f"{ie} compatible refl1d & bumps packages not installed.")
     try:
         from refnx.analysis import Parameter as refnxParameter
         from refnx.analysis.parameter import _BinaryOp
+
         type ParameterLike = int | float | refnxParameter | _BinaryOp
     except ImportError as ie:
         print(f"{ie} compatible refnx package not installed.")
-        
+
+
+# define a type for user defined SLDConstraints to follow.
+class SLDConstraintType(Protocol):
+    def __init__(self, required_pars: dict[str, ParameterLike]) -> None: ...
+    def layer_choices(self) -> list[int]: ...
+    def __call__(
+        self, layer_integrals: list[float]
+    ) -> tuple[list[int], list[float]]: ...
